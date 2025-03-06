9fcf9c0e9ed91c7efdf5e8c6758a7b8d941e76fc
Cleiton <cleiton.correa@compasso.com.br>
2025-03-06 12:52:12 -0300

    Alteração no servidor TS

--- src/main.server.ts
+++ src/main.server.ts
@@ -3,5 +3,9 @@ import { AppComponent } from './app/app.component';
 import { config } from './app/app.config.server';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
+export const environment = {
+  production: false,
+  apiUrl: 'http://localhost:8080/api/v1',
+};
 
 export default bootstrap;
